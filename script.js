342dc7154f636baadbd536fadf2db3408bbc879d
Ryguy <ryan70297@gmail.com>
2022-11-30 22:42:17 -0400

    added forecast and updated ui

--- script.js
+++ script.js
@@ -1,66 +1,101 @@
 var userInput = document.getElementById("input");
 var searchBtn = document.getElementById("search");
-var searchResult = document.getElementById("results");
 var searchedCity = document.getElementById("searched")
 var cityText = document.getElementById("city");
 
 var today = dayjs().format('M/DD/YYYY')
 
 
-
 searchBtn.addEventListener("click", function (){
 searchValue = userInput.value;
 getWeather(searchValue);
+if  (searchValue !== "")  {
+  places = []
+  var places = JSON.parse(localStorage.getItem("places")) || [];
+  places.push(searchValue);
+  localStorage.setItem("places", JSON.stringify(places));
 
+}
+})
+
+
+searchedCity.addEventListener("click", function (event) {
+  searchValue = event.target.getAttribute("data")
+  console.log(searchValue)
+  getWeather(searchValue)
 })
 
 
 
 
 function getWeather(searchValue) {
-$("#results").empty()
-    // var searchValue = userInput.value;
+  $("#fiveday").empty()
+  $("#today").empty()
 console.log(searchValue);
 
-var apiurl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=metric" + "&appid=2819d50096838c6a50fa89bab141bf6e";
+var oneDay = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=metric" + "&appid=40aac2ee5b1002304c21fb110fc31e01";
 
-fetch(apiurl)
+fetch(oneDay)
 .then(function (response) {
   if (response.ok) {
     console.log(response);
     response.json().then(function (data) {
       console.log(data);
-      places = []
-      var places = JSON.parse(localStorage.getItem("places")) || [];
-      places.push(searchValue);
-      localStorage.setItem("places", JSON.stringify(places));
-
-
-
+     
       var iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     
- $("#results").append(
-  "<div class='col'>"
-  + "<h1>" + data.name + " - " + today + "<img src=" + iconUrl + ">" + "</h1>"
+ $("#today").append(
+   "<h1 class='text-center'>" + data.name + "</h1>"
+  +"<div class='col text-center bg-info m-3'>"
+  + "<h3>" +  "Today" + "<br>" + today +  "</h3>" 
+  + "<img src=" + iconUrl + ">"
   +  "<ul>" + "Temperature: " +  Math.round(data.main.temp) + "°C" + "</ul>"
   +  "<ul>" + "Humidity: " + data.main.humidity + "%" + "</ul>"
-  +  "<ul>" + "Wind Speed: " +  data.wind.speed + "km/h" + "</ul>"
+  +  "<ul>" + "Wind Speed: " +  Math.round(data.wind.speed) + " " + "km/h" + "</ul>"
   + "</div>"
+  +"<h1 class = 'text-center'>" + "Five Day Forecast" +  "</h1>"
  )
+     
+var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&units=metric" + "&appid=40aac2ee5b1002304c21fb110fc31e01";
+
+ fetch(fiveDay)
+  .then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (fiveData) {
+        console.log(fiveData);
       
 
-     getPlaces()
-    
-})}else {
-    alert('Error: ' + response.statusText);
-  }
-  
-})
-  .catch(function (error) {
-    alert('Unable to connect to OpenWeather');
-  });
+for (let i = 0; i < fiveData.list.length; i++) {
 
-}; 
+if (fiveData.list[i].dt_txt.includes("12:00:00") ) {
+
+var date = dayjs.unix(fiveData.list[i].dt).format('M/DD/YYYY')  
+var day = dayjs.unix(fiveData.list[i].dt).format('dddd')
+var iconUrl = "http://openweathermap.org/img/w/" + fiveData.list[i].weather[0].icon + ".png"; 
+    
+$("#fiveday").append(
+  
+   "<div class='col text-center bg-info m-3'>"
+  + "<h3>" + day + "<br>" + date + "</h3>"
+  + "<img src=" + iconUrl + ">"
+  + "<ul>" + "Temperature: " +  Math.round(fiveData.list[i].main.temp) + "°C" + "</ul>"
+  + "<ul>" + "Humidity: " + fiveData.list[i].main.humidity + "%" + "</ul>"
+  + "</div>"
+ 
+)
+        }
+        }
+
+        getPlaces()
+      })
+    }
+  })
+    })
+  }
+})
+
+}
 
 
 
@@ -70,8 +105,7 @@ function getPlaces() {
   var places = JSON.parse(localStorage.getItem("places")) || [];
   for (let index = 0; index < places.length; index++) {
     var searchedPlace = document.createElement("button")
-searchedPlace.classList.add("btn" ,"btn-primary", "btn-lg",  "w-100" , "mb-2" ,"place-btn")
-
+searchedPlace.classList.add("btn" ,"btn-primary", "btn-md",  "w-100" , "mb-2" ,"place-btn")
 searchedPlace.textContent = places[index]
 searchedPlace.setAttribute("data", places[index])
 searchedCity.append(searchedPlace) 
@@ -91,10 +125,3 @@ clearBtn.addEventListener("click", function () {
 }
   
 getPlaces()
-
-
-$(".place-btn").on("click" , function(){
-  var searchValue = $(this).attr("data")
-  console.log(searchValue)
-  getWeather(searchValue)
-})
